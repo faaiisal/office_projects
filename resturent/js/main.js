034c4622d1e09edb4765d20385f106736dc8cf1a
ummayhoneysara <sara.ummay@gmail.com>
2017-08-06 11:55:13 +0600

    Portfolio Projects

---
+++ resturent/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $('#menu').slicknav();
+    $(".post").fitVids();
+});
+(function($) {
+    "use strict";
+    jQuery(document).ready(function($){
+        $("#sticker").sticky({topSpacing: 0});
+    });
+    jQuery(window).load(function(){
+        
+    });
+    
+    }(jQuery));
